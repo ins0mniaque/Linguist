6c4fefd300b211ab3d1970b0f05b2a6ef9cd98ed
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2019-07-06 20:17:03 -0400

    Added code generators and commands

---
+++ Localizer/Generators/LocalizerCodeGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.CodeDom;
+using System.Runtime.InteropServices;
+
+namespace Localizer
+{
+    [ Guid ( "86CFFC91-69EF-44B7-83EE-937ED31C9054" ) ]
+    public sealed class LocalizerCodeGenerator : LocalizerCodeGeneratorBase
+    {
+        public const string Name        = nameof ( LocalizerCodeGenerator );
+        public const string Description = "Generates a public strongly-typed resource class for localization.";
+
+        protected override MemberAttributes AccessModifier => MemberAttributes.Public;
+    }
+}
---
+++ Localizer/Generators/LocalizerCodeGeneratorBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Text;
+
+using Microsoft.VisualStudio.Designer.Interfaces;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TextTemplating.VSHost;
+
+namespace Localizer
+{
+    public abstract class LocalizerCodeGeneratorBase : BaseCodeGeneratorWithSite
+    {
+        protected abstract MemberAttributes AccessModifier { get; }
+
+        public override string GetDefaultExtension ( )
+        {
+            var extension = CodeDomProvider.FileExtension;
+            if ( extension?.Length > 0 && extension [ 0 ] != '.' )
+                extension = "." + extension;
+
+            return extension;
+        }
+
+        protected override byte [ ] GenerateCode ( string inputFileName, string inputFileContent )
+        {
+            var code = "// Generated code test";
+ 
+            return code != null ? Encoding.UTF8.GetBytes ( code ) : null;
+        }
+
+        private static readonly Guid CodeDomServiceGuid = new Guid ( "73E59688-C7C4-4A85-AF64-A538754784C5" );
+
+        private CodeDomProvider codeDomProvider;
+        private CodeDomProvider CodeDomProvider
+        {
+            get
+            {
+                if ( codeDomProvider == null )
+                {
+                    var vsmdCodeDomProvider = (IVSMDCodeDomProvider) GetService ( CodeDomServiceGuid ) ??
+                                              throw new ServiceUnavailableException ( typeof ( IVSMDCodeDomProvider ) );
+
+                    codeDomProvider = vsmdCodeDomProvider.CodeDomProvider as CodeDomProvider ??
+                                      throw new ServiceUnavailableException ( typeof ( CodeDomProvider ) );
+                }
+
+                return codeDomProvider;
+            }
+        }
+    }
+}
