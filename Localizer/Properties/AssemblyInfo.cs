f32c038e2e1c99c7c00f8fde57a2d9f5a588f5cb
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2019-07-09 22:55:23 -0400

    Renamed Localizer.Linguistics project to Localizer

--- Localizer/Properties/AssemblyInfo.cs
+++ Localizer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Localizer.Linguistics")]
+[assembly: AssemblyTitle ( "Localizer" )]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Localizer.Linguistics")]
+[assembly: AssemblyProduct ( "Localizer" )]
 [assembly: AssemblyCopyright ( "" )]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
