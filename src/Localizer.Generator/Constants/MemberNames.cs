24f22bcf977efb149f220e7063ce8b52f9bf99ad
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2019-07-15 20:22:04 -0400

    Fixed generated code member names for case-insensitive languages

--- src/Localizer.Generator/Constants/MemberNames.cs
+++ src/Localizer.Generator/Constants/MemberNames.cs
@@ -3,17 +3,17 @@
     internal static class MemberNames
     {
         public const string ResourceManagerPropertyName      = "ResourceManager";
-        public const string ResourceManagerFieldName         = "resourceManager";
+        public const string ResourceManagerFieldName         = "_resourceManager";
         public const string SingletonFieldName               = "Value";
         public const string CultureInfoPropertyName          = "Culture";
-        public const string CultureInfoFieldName             = "culture";
+        public const string CultureInfoFieldName             = "_culture";
         public const string NotifyCultureChangedMethodName   = "NotifyCultureChanged";
         public const string NotifyCultureChangedVariableName = "notify";
         public const string FormatMethodSuffix               = "Format";
         public const string FormatMethodParameterName        = "arg{0}";
         public const string ResourceKeyEnumName              = "ResourceKey";
-        public const string ResourceKeyTranslatorFieldName   = "resourceKey";
-        public const string ResourceKeyTranslatorName        = "NameOf";
+        public const string ResourceKeyTranslatorFieldName   = "_resourceKey";
+        public const string ResourceKeyTranslatorName        = "ResourceKeyToName";
         public const string ResourceKeyParameterName         = "key";
     }
 }
