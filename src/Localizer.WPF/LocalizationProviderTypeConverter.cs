e2113f83aebc82715dbb7e12d48af452b0ea72ee
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2019-07-21 17:57:03 -0400

    Added LocalizationProviderTypeConverter cache

--- src/Localizer.WPF/LocalizationProviderTypeConverter.cs
+++ src/Localizer.WPF/LocalizationProviderTypeConverter.cs
@@ -9,6 +9,8 @@ namespace Localizer.WPF
 {
     public class LocalizationProviderTypeConverter : TypeConverter
     {
+        private static Cache < string, ILocalizationProvider > cache = new Cache < string, ILocalizationProvider > ( );
+
         public override bool CanConvertFrom ( ITypeDescriptorContext context, Type sourceType )
         {
             return sourceType == typeof ( string );
@@ -16,14 +18,22 @@ namespace Localizer.WPF
  
         public override object ConvertFrom ( ITypeDescriptorContext context, CultureInfo culture, object value )
         {
-            if ( value is string name )
+            if ( value is string namedProvider )
             {
-                var assembly = Assembly.Load ( name.Split ( '.' ).First ( ) );
+                if ( ! cache.TryGet ( namedProvider, out var provider ) )
+                    cache.Add ( namedProvider, provider = LoadNamedProvider ( namedProvider ) );
 
-                return new ResourceManagerLocalizationProvider ( new ResourceManager ( name, assembly ) );
+                return provider;
             }
 
             return base.ConvertFrom ( context, culture, value );
+        }
+
+        private static ILocalizationProvider LoadNamedProvider ( string name )
+        {
+            var assembly = Assembly.Load ( name.Split ( '.' ).First ( ) );
+
+            return new ResourceManagerLocalizationProvider ( new ResourceManager ( name, assembly ) );
         }
     }
 }
