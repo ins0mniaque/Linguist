7a50c5a86bed06886fedc8a1028b8c698d13a84d
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2019-07-21 22:29:11 -0400

    Added automatic key WPF demo

---
+++ samples/Localizer.WPF.Demo/Demos/AutomaticKeyDemo.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Localizer.WPF.Demo
+{
+    public partial class AutomaticKeyDemo : UserControl
+    {
+        public AutomaticKeyDemo ( )
+        {
+            InitializeComponent ( );
+        }
+    }
+}
--- samples/Localizer.WPF.Demo/Properties/Resources.Designer.cs
+++ samples/Localizer.WPF.Demo/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Localizer.WPF.Demo.Properties
-{
-
-
+namespace Localizer.WPF.Demo.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,170 @@ namespace Localizer.WPF.Demo.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute ( "System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0" )]
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute ( )]
-    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute ( )]
-    internal class Resources
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    public class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
-        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute ( "Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode" )]
-        internal Resources ( )
-        {
+        
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
-        [global::System.ComponentModel.EditorBrowsableAttribute ( global::System.ComponentModel.EditorBrowsableState.Advanced )]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ( ( resourceMan == null ) )
-                {
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        public static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Localizer.WPF.Demo.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
-        [global::System.ComponentModel.EditorBrowsableAttribute ( global::System.ComponentModel.EditorBrowsableState.Advanced )]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        public static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {0} items.
+        /// </summary>
+        public static string AutomaticKeyDemo_FormattedTextBlock_Text {
+            get {
+                return ResourceManager.GetString("AutomaticKeyDemo.FormattedTextBlock.Text", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {0} item.
+        /// </summary>
+        public static string AutomaticKeyDemo_FormattedTextBlock_Text_One {
+            get {
+                return ResourceManager.GetString("AutomaticKeyDemo.FormattedTextBlock.Text.One", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to No items.
+        /// </summary>
+        public static string AutomaticKeyDemo_FormattedTextBlock_Text_Zero {
+            get {
+                return ResourceManager.GetString("AutomaticKeyDemo.FormattedTextBlock.Text.Zero", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to PaleGreen.
+        /// </summary>
+        public static string AutomaticKeyDemo_LayoutRoot_Background {
+            get {
+                return ResourceManager.GetString("AutomaticKeyDemo.LayoutRoot.Background", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Text value for NamedTextBlock.
+        /// </summary>
+        public static string AutomaticKeyDemo_NamedTextBlock_Text {
+            get {
+                return ResourceManager.GetString("AutomaticKeyDemo.NamedTextBlock.Text", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Pluralization support: Try to enter numerical values such as 0, 1, 3.14....
+        /// </summary>
+        public static string AutomaticKeyDemo_PluralHint_Text {
+            get {
+                return ResourceManager.GetString("AutomaticKeyDemo.PluralHint.Text", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Text value for control type TextBlock.
+        /// </summary>
+        public static string AutomaticKeyDemo_TextBlock_Text {
+            get {
+                return ResourceManager.GetString("AutomaticKeyDemo.TextBlock.Text", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Automatic keys.
+        /// </summary>
+        public static string MainWindow_AutomaticKeyDemo_Header {
+            get {
+                return ResourceManager.GetString("MainWindow.AutomaticKeyDemo.Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Keys.
+        /// </summary>
+        public static string MainWindow_KeyDemo_Header {
+            get {
+                return ResourceManager.GetString("MainWindow.KeyDemo.Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Bound keys.
+        /// </summary>
+        public static string MainWindow_KeyPathDemo_Header {
+            get {
+                return ResourceManager.GetString("MainWindow.KeyPathDemo.Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Window language.
+        /// </summary>
+        public static string MainWindow_Language_Label_Text {
+            get {
+                return ResourceManager.GetString("MainWindow.Language.Label.Text", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Without custom tool.
+        /// </summary>
+        public static string MainWindow_NoCustomToolDemo_Header {
+            get {
+                return ResourceManager.GetString("MainWindow.NoCustomToolDemo.Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Providers.
+        /// </summary>
+        public static string MainWindow_ProviderDemo_Header {
+            get {
+                return ResourceManager.GetString("MainWindow.ProviderDemo.Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Localizer WPF Demo.
+        /// </summary>
+        public static string MainWindow_Title {
+            get {
+                return ResourceManager.GetString("MainWindow.Title", resourceCulture);
             }
         }
     }
