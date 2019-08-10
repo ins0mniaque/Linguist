71cf1a33c3620f1506d9c2efe3f8abaf71f1b834
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2019-08-10 18:16:53 -0400

    Added template engine

---
+++ src/Linguist/TemplateEngine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Linguist
+{
+    public class TemplateEngine : Dictionary < string, string >
+    {
+        private static readonly Regex engine = new Regex ( @"\{(.+?)\}", RegexOptions.Compiled );
+
+        public TemplateEngine ( string template ) : base ( StringComparer.InvariantCultureIgnoreCase )
+        {
+            Template = template;
+        }
+
+        public string Template { get; set; }
+
+        public override string ToString ( )
+        {
+            return engine.Replace ( Template, ReplaceArgument );
+        }
+
+        private string ReplaceArgument ( Match match )
+        {
+            var name = match.Groups [ 1 ].Value;
+            if ( TryGetValue ( name, out var value ) )
+                return value;
+
+            return match.Value;
+        }
+    }
+}
