0b7d0bfafc7c339b5013fd6902f50edeb9e4afed
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2019-08-18 20:37:17 -0400

    Added Linguist.MSBuild project

---
+++ samples/Linguist.MSBuild.Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linguist.MSBuild.Demo
+{
+    class Program
+    {
+        static void Main ( string [ ] args )
+        {
+            Console.WriteLine ( "Hello World!" );
+        }
+    }
+}
---
+++ src/Linguist.MSBuild/Localization.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.CodeDom.Compiler;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Linguist.MSBuild
+{
+    public class Localization : Task
+    {
+        [ Required ] public ITaskItem [ ] Localizations     { get; set; }
+        [ Required ] public ITaskItem     Language          { get; set; }
+                     public ITaskItem [ ] References        { get; set; }
+        [ Output   ] public ITaskItem [ ] Compile           { get; set; }
+        [ Output   ] public ITaskItem [ ] EmbeddedResources { get; set; }
+
+        public override bool Execute ( )
+        {
+            if ( Language == null || string.IsNullOrEmpty ( Language.ItemSpec ) )
+            {
+                Log.LogError ( "Missing Language PropertyItem definition in project to specify which CodeDomProvider to use.  e.g. <Language>C#</Language>" );
+                return false;
+            }
+
+            // System.Diagnostics.Debugger.Launch ( );
+
+            Log.LogMessage ( MessageImportance.High, "Hello World!" );
+ 
+            var provider = CreateCodeDomProvider ( Language.ItemSpec );
+            if ( provider == null )
+                return false;
+
+            return true;
+        }
+
+        private CodeDomProvider CreateCodeDomProvider ( string language )
+        {
+            foreach ( var compiler in CodeDomProvider.GetAllCompilerInfo ( ) )
+                foreach ( var compilerLanguage in compiler.GetLanguages ( ) )
+                    if ( string.Equals ( language, compilerLanguage, StringComparison.OrdinalIgnoreCase ) )
+                        return CodeDomProvider.CreateProvider ( compilerLanguage );
+
+            Log.LogError ( $"Unable to find a CodeDomProvider for language { language }." );
+
+            return null;
+        }
+    }
+}
